ca5d39951ee4c93053bc75673c1d61a89cbf3d30
Thomas Walker <thomasedwalker@gmail.com>
2019-11-30 23:30:07 -0700

    Early version of custom beacon display for the nearby beacons page.

---
+++ app/src/main/java/com/glimpse/glimpse/ui/BeaconButton.kt
@@ -0,0 +1,12 @@
+package com.glimpse.glimpse.ui
+
+import android.content.Context
+import android.widget.Button
+
+class BeaconButton : Button {
+
+    constructor(context : Context) : super(context) {
+
+    }
+
+}
